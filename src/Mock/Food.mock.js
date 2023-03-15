cb1ae2a4a248e4f449f4f00a2ae8baad3b4235b4
abibritto <abinadabebrito@gmail.com>
2023-03-15 16:30:09 -0300

    feat: mock criado

---
+++ src/Mock/Food.mock.js
@@ -0,0 +1,50 @@
+export const MOCK_FOOD = [
+  {
+    id: 1,
+    imgPath: "burguer.svg",
+    imgPathDetail: "../../../public/burguer.svg",
+    title: "X-Burguer",
+    description: "Hambúrguer de carne bovina grelhado, queijo, alface, tomate e maionese, servido em pão de hambúrguer.",
+    price: 15.99
+  },
+  {
+    "id": 2,
+    imgPath: "acai.svg",
+    imgPathDetail: "../../../public/acai.svg",
+    title: "Açaí com mix de frutas",
+    description: "Tigela de açaí batido com banana, morango, granola, coco ralado e mel.",
+    price: 12.99
+  },
+  {
+    id: 3,
+    imgPath: "frango.svg",
+    imgPathDetail: "../../../public/frango.svg",
+    title: "Frango frito crocante",
+    description: "Tiras de frango empanadas e fritas, servidas com molho barbecue.",
+    price: 18.99
+  },
+  {
+    id: 4,
+    imgPath: "pastel.svg",
+    imgPathDetail: "../../../public/pastel.svg",
+    title: "Pastel",
+    description: "Pastel de carne moída com temperos e queijo, frito até ficar crocante.",
+    price: 5.99
+  },
+  {
+    id: 5,
+    imgPath: "pizza.svg",
+    imgPathDetail: "../../../public/pizza.svg",
+    title: "Pizza",
+    description: "Pizza de queijo mussarela com molho de tomate, orégano e azeitonas, servida em fatias.",
+    price: 25.99
+  },
+  {
+    id: 6,
+    imgPath: "biscoito.svg",
+    imgPathDetail: "../../../public/biscoito.svg",
+    title: "Biscoitos",
+    description: "Biscoitos caseiros de chocolate com gotas de chocolate belga em fromato de coração.",
+    price: 8.99
+  }
+]
