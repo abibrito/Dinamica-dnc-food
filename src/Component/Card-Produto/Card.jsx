f5bb7b6c4f56537b52591f24d2d7a88cf721fa76
abibritto <abinadabebrito@gmail.com>
2023-03-15 16:29:35 -0300

    fix: ajustando card na tela

--- src/Component/Card-Produto/Card.jsx
+++ src/Component/Card-Produto/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./index.scss";
+import img from "../../../public/burguer.svg"
 
 const Card = () => {
   return (
     <div className="card">
-      <img src="https://example.com/image.png" alt="Imagem do card" />
+      <img src={img} alt="Imagem do card" />
       <div className="card-body">
         <h5 className="card-title">Título do Card</h5>
         <button className="btn btn-primary">Adicionar</button>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,25 @@
-import Header from "../../Component/Header/Header"
-import Card from "../../Component/Card-Produto/Card"
+import Header from "../../Component/Header/Header";
+import Card from "../../Component/Card-Produto/Card";
+import "./index.scss";
 
 const Home = () => {
   return (
-    <Header/>
-  )
-}
+    <div>
+      <Header />
+      <div className="Display-card">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="Display-card">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="Footer">
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
