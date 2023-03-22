48d62b58e2b0127ed7a94c3dbb46a53d712dc00f
abibritto <abinadabebrito@gmail.com>
2023-03-21 22:57:31 -0300

    fix: rota product

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {  createBrowserRouter,  RouterProvider } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import './index.scss'
+import "./index.scss";
+import { MOCK_FOOD } from "../src/Mock/Food.mock";
+import Product from "./Pages/Product/Product";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home data={MOCK_FOOD} />,
+  },
+  {
+    path: "/",
+    element: <Product data={MOCK_FOOD} />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
